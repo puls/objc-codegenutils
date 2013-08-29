fe6d39feea0247499df0cf55b3fa73ab67a27438
Jim Puls <puls@squareup.com>
2013-08-29 15:08:16 -0700

    Initial Commit

---
+++ assetgen/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  assetgen
+//
+//  Created by Jim Puls on 8/29/13.
+//  Copyright (c) 2013 Square, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
