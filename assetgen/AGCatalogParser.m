39d5d77790657bb95c79835e51242165700a7fe4
Jim Puls <puls@squareup.com>
2013-08-29 21:55:50 -0700

    Simplify the generated code a bit; support non-resizable images.

--- assetgen/AGCatalogParser.m
+++ assetgen/AGCatalogParser.m
@@ -83,7 +83,31 @@
     if (!contents) {
         return;
     }
-    
+
+    // Sort the variants: retina4 comes first, then iphone/ipad-specific, then universal
+    // Within each group, 2x comes before 1x
+    NSArray *variants = [contents[@"images"] sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        if (![obj1[@"subtype"] isEqualToString:obj2[@"subtype"]]) {
+            if (obj1[@"subtype"]) {
+                return NSOrderedAscending;
+            }
+            if (obj2[@"subtype"]) {
+                return NSOrderedDescending;
+            }
+        }
+        
+        if (![obj1[@"idiom"] isEqualToString:obj2[@"idiom"]]) {
+            if ([obj1[@"idiom"] isEqualToString:@"universal"]) {
+                return NSOrderedDescending;
+            }
+            if ([obj2[@"idiom"] isEqualToString:@"universal"]) {
+                return NSOrderedAscending;
+            }
+        }
+        
+        return -[obj1[@"scale"] compare:obj2[@"scale"]];
+    }];
+
     NSString *interface = [NSString stringWithFormat:@"+ (UIImage *)imageFor%@;\n", name];
     @synchronized(self.interfaceContents) {
         [self.interfaceContents addObject:interface];
@@ -92,47 +116,68 @@
     NSMutableString *implementation = [interface mutableCopy];
     [implementation appendString:@"{\n"];
     
-    [implementation appendFormat:@"    UIImage *cachedImage = [[self imageCache] objectForKey:@\"%@\"];\n", name];
-    [implementation appendString:@"    if (cachedImage) {\n"];
-    [implementation appendString:@"        return cachedImage;\n"];
-    [implementation appendString:@"    }\n\n"];
-    
-    for (NSDictionary *variant in contents[@"images"]) {
-        BOOL isUniversal = [variant[@"idiom"] isEqualToString:@"universal"];
-        NSString *indentation = @"";
-        if (!isUniversal) {
-            NSString *idiom = [variant[@"idiom"] isEqualToString:@"iphone"] ? @"UIUserInterfaceIdiomPhone" : @"UIUserInterfaceIdiomPad";
-            [implementation appendFormat:@"    if (UI_USER_INTERFACE_IDIOM() == %@) {\n", idiom];
-            indentation = @"    ";
-        }
-        
-        CGFloat scale = [variant[@"scale"] floatValue];
-        NSString *filename = [variant[@"filename"] stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"@%@", variant[@"scale"]] withString:@""];
-        [implementation appendFormat:@"%@    if ([UIScreen mainScreen].scale == %.1ff) {\n", indentation, scale];
-        [implementation appendFormat:@"%@        UIImage *baseImage = [UIImage imageNamed:@\"%@\"];\n", indentation, filename];
-
-        NSDictionary *resizing = variant[@"resizing"];
-        if (resizing) {
-            CGFloat top = [resizing[@"capInsets"][@"top"] floatValue] / scale;
-            CGFloat left = [resizing[@"capInsets"][@"left"] floatValue] / scale;
-            CGFloat bottom = [resizing[@"capInsets"][@"bottom"] floatValue] / scale;
-            CGFloat right = [resizing[@"capInsets"][@"right"] floatValue] / scale;
-            NSString *mode = [resizing[@"center"][@"mode"] isEqualToString:@"stretch"] ? @"UIImageResizingModeStretch" : @"UIImageResizingModeTile";
-            
-            [implementation appendFormat:@"%@        return [baseImage resizableImageWithCapInsets:UIEdgeInsetsMake(%.1ff, %.1ff, %.1ff, %.1ff) resizingMode:%@];\n", indentation, top, left, bottom, right, mode];
-        } else {
-            [implementation appendFormat:@"%@        return baseImage;\n", indentation];
-        }
-
-        [implementation appendFormat:@"%@    }\n", indentation];
-        
-        if (!isUniversal) {
-            [implementation appendString:@"    }\n"];
+    // If there are only one or two variants and they only differ by 1x or 2x and they're not resizable, short circuit
+    BOOL shortCircuit = (variants.count == 1);
+    if (variants.count == 2) {
+        if (!variants[0][@"resizing"] && !variants[1][@"resizing"]) {
+            NSString *filename1 = [variants[0][@"filename"] stringByReplacingOccurrencesOfString:@"@2x" withString:@""];
+            NSString *filename2 = [variants[1][@"filename"] stringByReplacingOccurrencesOfString:@"@2x" withString:@""];
+            shortCircuit = [filename1 isEqualToString:filename2];
         }
     }
+    if (shortCircuit) {
+        [implementation appendFormat:@"    return [UIImage imageNamed:@\"%@\"];\n", [variants lastObject][@"filename"]];
+        [implementation appendString:@"}\n"];
+
+    } else {
+        [implementation appendFormat:@"    UIImage *image = [[self imageCache] objectForKey:@\"%@\"];\n", name];
+        [implementation appendString:@"    if (image) {\n"];
+        [implementation appendString:@"        return image;\n"];
+        [implementation appendString:@"    }\n\n"];
+        
+        for (NSDictionary *variant in variants) {
+            if (!variant[@"filename"]) {
+                continue;
+            }
+            BOOL isUniversal = [variant[@"idiom"] isEqualToString:@"universal"];
+            NSString *indentation = @"";
+            if (!isUniversal) {
+                NSString *idiom = [variant[@"idiom"] isEqualToString:@"iphone"] ? @"UIUserInterfaceIdiomPhone" : @"UIUserInterfaceIdiomPad";
+                [implementation appendFormat:@"    if (UI_USER_INTERFACE_IDIOM() == %@) {\n", idiom];
+                indentation = @"    ";
+            }
+            
+            CGFloat scale = [variant[@"scale"] floatValue];
+            NSString *filename = [variant[@"filename"] stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"@%@", variant[@"scale"]] withString:@""];
+            NSString *scaleIndentation = [indentation stringByAppendingString:@"    "];
+            [implementation appendFormat:@"%@if ([UIScreen mainScreen].scale == %.1ff) {\n", scaleIndentation, scale];
+            [implementation appendFormat:@"%@    image = [UIImage imageNamed:@\"%@\"];\n", scaleIndentation, filename];
+            
+            NSDictionary *resizing = variant[@"resizing"];
+            if (resizing) {
+                CGFloat top = [resizing[@"capInsets"][@"top"] floatValue] / scale;
+                CGFloat left = [resizing[@"capInsets"][@"left"] floatValue] / scale;
+                CGFloat bottom = [resizing[@"capInsets"][@"bottom"] floatValue] / scale;
+                CGFloat right = [resizing[@"capInsets"][@"right"] floatValue] / scale;
+                NSString *mode = [resizing[@"center"][@"mode"] isEqualToString:@"stretch"] ? @"UIImageResizingModeStretch" : @"UIImageResizingModeTile";
+                
+                [implementation appendFormat:@"%@    image = [image resizableImageWithCapInsets:UIEdgeInsetsMake(%.1ff, %.1ff, %.1ff, %.1ff) resizingMode:%@];\n", scaleIndentation, top, left, bottom, right, mode];
+            }
+            
+            [implementation appendFormat:@"%@}\n", scaleIndentation];
+            
+            if (!isUniversal) {
+                [implementation appendFormat:@"%@}\n", indentation];
+            }
+            
+            [implementation appendString:@"\n"];
+        }
+        
+        [implementation appendFormat:@"    [[self imageCache] setObject:image forKey:@\"%@\"];\n", name];
+        [implementation appendString:@"    return image;\n"];
+        [implementation appendString:@"}\n"];
+    }
     
-    [implementation appendString:@"    return nil;\n"];
-    [implementation appendString:@"}\n"];
     @synchronized(self.implementationContents) {
         [self.implementationContents addObject:implementation];
     }
