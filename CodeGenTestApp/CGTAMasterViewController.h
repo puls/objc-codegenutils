a1bc38023a500c70615fb65371d861bcd618093e
Jim Puls <puls@squareup.com>
2014-02-10 14:15:20 -0800

    Licensing.

--- CodeGenTestApp/CGTAMasterViewController.h
+++ CodeGenTestApp/CGTAMasterViewController.h
@@ -3,8 +3,9 @@
 //  CodeGenTestApp
 //
 //  Created by Jim Puls on 2/3/14.
-//  Copyright (c) 2014 Square, Inc. All rights reserved.
-//
+//  Licensed to Square, Inc. under one or more contributor license agreements.
+//  See the LICENSE file distributed with this work for the terms under
+//  which Square, Inc. licenses this file to you.
 
 
 @interface CGTAMasterViewController : UICollectionViewController
--- Shared/CGUCodeGenTool.m
+++ Shared/CGUCodeGenTool.m
@@ -1,10 +1,11 @@
 //
-//  CGCodeGenTool.m
+//  CGUCodeGenTool.m
 //  codegenutils
 //
 //  Created by Jim Puls on 9/6/13.
-//  Copyright (c) 2013 Square, Inc. All rights reserved.
-//
+//  Licensed to Square, Inc. under one or more contributor license agreements.
+//  See the LICENSE file distributed with this work for the terms under
+//  which Square, Inc. licenses this file to you.
 
 #import "CGUCodeGenTool.h"
 
