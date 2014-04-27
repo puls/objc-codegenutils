fdaab76c9201b36f7fb9022e33d9bc94f097c8a3
Tony Arnold <tony@thecocoabots.com>
2014-04-27 13:34:51 +1000

    Add a tool to dump identifier constants from XIBs

---
+++ xibconstants/IDXIBDumper.h
@@ -0,0 +1,14 @@
+//
+//  IDXIBDumper.h
+//  codegenutils
+//
+//  Created by Tony Arnold on 27/04/2014.
+//  Licensed to Square, Inc. under one or more contributor license agreements.
+//  See the LICENSE file distributed with this work for the terms under
+//  which Square, Inc. licenses this file to you.
+
+#import "CGUCodeGenTool.h"
+
+@interface IDXIBDumper : CGUCodeGenTool
+
+@end
